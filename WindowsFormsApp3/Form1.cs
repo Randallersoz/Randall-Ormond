a2ec661aef56dbb1de7fa2a9620af56d7d110a31
ersozormondllc@gmail.com <ersozormondllc@gmail.com>
2019-11-26 16:40:53 -0800

    initial

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    using System;
+    using System.Windows.Forms;
+    using System.Runtime.InteropServices;
+    using Excel = Microsoft.Office.Interop.Excel;
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+
+            double dbl;
+            double[] track = new double[10];
+            int counter = 0;
+            string str;
+            int rCnt;
+            int cCnt;
+            int rw = 0;
+            int cl = 0;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(@"d:\csharp-Excel.xls", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;
+            cl = range.Columns.Count;
+
+            for (rCnt = 1; rCnt <= rw; rCnt++)
+            {
+                for (cCnt = 1; cCnt <= cl; cCnt++)
+                {
+                    dbl = (double)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
+                    track[counter] = dbl;
+                    str = dbl.ToString();
+                    MessageBox.Show(str);
+                    counter++;
+                }
+            }
+
+            for (counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine("Recorded[{0}] = {1}", counter, track[counter]);
+                Console.WriteLine("Average is ");
+            }
+
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+        }
+    }
+}
